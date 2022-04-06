30d13e47913d36309943265348d1c9ccffbba016
João paulo C <joaopaulo33573@gmail.com>
2022-04-06 15:53:21 -0300

    Using SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import {stripe} from '../services/stripe';
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss'
-export default function Home(){
+
+interface HomeProps{
+    product:{
+        priceId:string;
+        amount: number;
+    }
+}
+
+export default function Home({product}:HomeProps){
     return(
         <>
             <Head>
@@ -13,12 +23,39 @@ export default function Home(){
                     <span> Hey, welcome</span>
                     <h1>News about the <span> React </span> world</h1>
                     <p>Get acess to all the publications <br />
-                     <span>for $9.90 month</span>
+                     <span>for {product.amount} month</span>
                     </p>
-                    <SubscribeButton/>
+                    <SubscribeButton priceId={product.priceId}/>
                 </section>
                 <img src="/images/avatar.svg" alt="Girl coding" />
             </main>
         </>
     )
+
+
+}
+// Aqui está rodando no servidor next  
+export const getStaticProps: GetStaticProps = async ()=> {
+    //Como estamos usando o stripe aqui, é só dar um pornto para ver as funções disponíveis 
+    //retrive quer dizer que será só um
+    const price = await stripe.prices.retrieve('price_1KiHQoFCWcZ6E7n9fKwSLt4c',{
+        //Faz isso pois o price do produto vem com o id do produto, mas queremos outras informações também(mas nem vamos usar, ele removeu)
+        expand:['product']
+    })
+ 
+    const product = {
+        priceId: price.id,
+        //valor unitário(vem sempre em centavos)
+        amount: new Intl.NumberFormat('en-US',{
+            style:'currency',
+            currency:'USD',
+        }).format(price.unit_amount/100),
+    }
+    //tudo que eu der retorno como propriedade, eu posso acessar lá no componente Home
+    return{
+        props:{
+            product,
+        },
+        revalidate:60 * 60 * 24, //24 hours
+    } 
 }
---
+++ src/services/stripe.ts
@@ -0,0 +1,13 @@
+import Stripe from 'stripe'
+import { version } from '../../package.json'
+export const stripe = new Stripe(
+    process.env.STRIPE_API_KEY,
+    {
+        apiVersion:'2020-08-27',
+        //informações de metadados, não tem muita importância   
+        appInfo:{
+            name:'Ignews',
+            version
+        },
+    }
+)
