19c4a577ceab924bd4fe3d6447775961c9494369
joaopaulo33 <joaopaulo33573@gmail.com>
2022-06-09 10:03:09 -0300

    Ouvindo eventos

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,9 @@
+export async function saveSubscription(
+    subscriptionId: string,
+    customerId: string,
+){
+    //Buscar o usuário no banco do FaunaDB com o id custumerId
+    //Para isso precisamos criar um indice no Fauna
+    //Salvar os dados da subscription  no FaunaDB
+    console.log(subscriptionId, customerId,"lalalalalalalaaalalalalalalalaalal")
+}
